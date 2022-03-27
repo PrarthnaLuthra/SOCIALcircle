2e01617dc425808f76d6f46d1597cafd14c23d74
Prarthna <prarthnaluthra@gmail.com>
2022-03-27 09:03:53 +0530

    setup static files for layout

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,6 @@
-module.exports.home = function(req,res){
-    return res.end('<h1>Express is up for ChatterBox</h1>')
+module.exports.home = function(req, res){
+    
+    return res.render('home', {
+        title: "Home"
+    });
 }
--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app =express()
 const port =8001;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+//extract style and script from sub pages into the layout
+app.set('layout extractStyles',true)
+app.set('layout extractScripts',true)
 
 // use express router
-app.use('/' ,require('./routes'))
+app.use('/', require('./routes'));
 
 //setting up view engine
-app.use('view engine','ejs')
+app.set('view engine','ejs')
 app.set('views','./views')
+
+
 
 app.listen(port,function(err){
     if(err){
