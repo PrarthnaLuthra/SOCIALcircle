a23b49ce154641879599208fd0b8c1523333c52e
Prarthna <prarthnaluthra@gmail.com>
2022-03-28 15:07:06 +0530

    Displayed comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -6,6 +6,12 @@ module.exports.home = function (req, res) {
   //Populating the post for each object
   Post.find({})
     .populate("user")
+    .populate({
+      path:'comments',
+      populate:{
+        path:'user'
+      }
+    })
     .exec(function (err, posts) {
       return res.render("home", {
         title: "SOCIALcircle| Home",
