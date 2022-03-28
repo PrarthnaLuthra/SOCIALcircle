ce4ef3013a7c95d70de35bd6feb8b9b6fb76e950
Prarthna <prarthnaluthra@gmail.com>
2022-03-28 23:29:54 +0530

    converted controllers to async-await format

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,27 +1,27 @@
 const { populate } = require("../models/post");
 const Post = require("../models/post");
-const User =require('../models/user')
+const User = require("../models/user");
 
-module.exports.home = function (req, res) {
+module.exports.home = async function (req, res) {
   //Populating the post for each object
-  Post.find({})
-    .populate("user")
-    .populate({
-      path:'comments',
-      populate:{
-        path:'user'
-      }
-    })
-    .exec(function (err, posts) {
-      User.find({},function(err, users){
-        return res.render('home',{
-          title: 'SOCIALcircle|Home',
-          posts: posts,
-          all_users: users
+  try {
+    let posts = await Post.find({})
+      .populate("user")
+      .populate({
+        path: "comments",
+        populate: {
+          path: "user",
+        },
+      });
+    let users = await User.find({});
 
-        })
-      })
-
-      
+    return res.render("home", {
+      title: "SOCIALcircle|Home",
+      posts: posts,
+      all_users: users,
     });
+  } catch (err) {
+    console.log("Error", err);
+    return;
+  }
 };
